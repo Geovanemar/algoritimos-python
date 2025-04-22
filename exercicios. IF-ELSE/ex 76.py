0284ae70099e3e8c3083251a9efb31ee77a1fa29
Geovanemar <geovanemartins618@gmail.com>
2025-04-22 18:15:06 -0300

    continuação if-else 2

---
+++ exercicios. IF-ELSE/ex 76.py
@@ -0,0 +1,7 @@
+numero1= float(input("Digite um numero: "))
+
+if numero1 % 5 == 0 and numero1 % 7 == 0:
+    print("esse numero é multiplo de 5 e 7")
+
+else:
+    print("esse numero não é multiplo de 5 e 7")    
