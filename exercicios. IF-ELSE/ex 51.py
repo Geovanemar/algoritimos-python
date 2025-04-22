b30fcdf73f9097160c26bab3d7d7dc3c33b80619
Geovanemar <geovanemartins618@gmail.com>
2025-04-22 02:12:10 -0300

    if-else continuação

---
+++ exercicios. IF-ELSE/ex 51.py
@@ -0,0 +1,8 @@
+numero1= float(input("Digite um numero: "))
+
+if numero1 > 50 or numero1 < 60:
+    print(" esse numero é maior que 50 e menor que 60 ")
+
+else:
+    print(" esse numero não condiz ") 
+       
---
+++ exercicios. IF-ELSE/ex 57.py
@@ -0,0 +1,10 @@
+numero1= float(input("Digite um numero: "))
+numero2= float(input("Digite um numero: "))
+soma = numero1 + numero2
+
+if soma == 20 or soma > 50:
+    print("essa soma é igual 20 e ou maior que 50")
+
+else:
+    print("essa soma não condiz")
+
