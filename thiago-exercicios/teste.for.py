19a58a24e5194de3d51cde4eedb421616462021f
Geovanemar <geovanemartins618@gmail.com>
2025-08-15 21:42:14 -0400

    .

--- thiago-exercicios/teste.for.py
+++ thiago-exercicios/teste.for.py
@@ -1,2 +1,19 @@
-for i in range (0, 100, 2):
-    print(i)
+i = 0
+j = 0
+
+matriz = []
+lin = int(input("Digite a quantidade de linhas: "))
+col = int(input("Digite a quantidade de colunas: "))
+
+while i < lin:
+    linha = []
+    while j < col:
+        num = 1
+        linha.append(num)
+        j = j + 1
+    matriz.append(linha) 
+    i = i + 1
+    j = 0
+
+print(matriz)   
+
