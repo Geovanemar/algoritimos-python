b70e4e8f8f078d18e17a5cdc73b4fcc85e499334
Geovanemar <geovanemartins618@gmail.com>
2025-09-24 21:31:37 -0400

    24/09

---
+++ segundo-SM-Python/Class/Pessoa.py
@@ -0,0 +1,12 @@
+class Pessoa:
+    def __init__(self,nome,cpf,fone,email,cidade,estado):
+        self.nome = nome
+        self.cpf = cpf
+        self.fone = fone
+        self.email = email
+        self.cidade = cidade
+        self.estado = estado
+
+    def get_grade(self):
+        print(self.)
+
