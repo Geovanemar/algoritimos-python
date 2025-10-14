a00a13e810963187a2472300fbf987919785394b
Geovanemar <geovanemartins618@gmail.com>
2025-10-13 21:40:51 -0400

    13/10

---
+++ alunoscalebe dia 13.py
@@ -0,0 +1,14 @@
+class Aluno:
+    def __init__(self, nome, matricula):
+        self.nome = nome
+        self.matricula = matricula
+
+class Disciplina:
+    def __init__(self, nome_disc, cod,):
+        self.nome_disc = nome_disc
+        self.cod = cod
+
+class Nota:
+    def __init__(self, valor):
+        self.valor = valor        
+
