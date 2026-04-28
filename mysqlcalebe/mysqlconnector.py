9bb219fc07d1d354558a8befadfa48bf2717350f
Geovanemar <geovanemartins618@gmail.com>
2026-04-28 19:24:07 -0400

    mysql.conector

--- mysqlcalebe/mysqlconnector.py
+++ mysqlcalebe/mysqlconnector.py
@@ -25,14 +25,30 @@ cursor.execute("show databases")
  
 for banco in cursor.fetchall():
     print(banco)
-
-cursor.execute()
-('''
-    CREATE TABLE IF NOTS EXITS alunos(
-    id INT AUTO_INCREMENT PRIMARY KEY, nome VARCHAR(100) not null,
-    idade int not null) 
-  ''')    
-
+ 
+cursor.execute(
+    """
+    CREATE TABLE IF NOT EXISTS alunos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) not null,
+    idade int not null)
+    """)
+ 
+#comando insert para adicionar um aluno
+ 
 sql = "INSERT INTO alunos (nome, idade) VALUES (%s, %s)"
-
-valores =
+ 
+valores = ("João", 20)
+cursor.execute(sql, valores)
+ 
+#executar consulta para mostrar os alunos
+ 
+cursor.execute("SELECT * FROM alunos")
+resultados = cursor.fetchall()
+for linha in resultados:
+    print(linha)
+ 
+#fechar a conexao
+cursor.close()
+conexao.close()
+ 
